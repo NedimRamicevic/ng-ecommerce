82f2c42018ae4125e648576afeb2a84b78747b20
Nedim <mr.ramicevic@gmail.com>
2023-08-21 10:25:45 +0300

    talwind

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { SaleComponent } from './components/sale/sale.component';
+import { CartComponent } from './components/cart/cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'**',
+    redirectTo:'/home',
+    pathMatch:'full'
+  },
+  {
+    path:'home',
+    component:HomeComponent,
+  },
+  {
+    path:'sale',
+    component:SaleComponent,
+  },
+  {
+    path:'cart',
+    component:CartComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { CartComponent } from './components/cart/cart.component';
+import { SaleComponent } from './components/sale/sale.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CartComponent,
+    SaleComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
