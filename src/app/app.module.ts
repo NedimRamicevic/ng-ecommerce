9bc9396d7402dfde3071c8528e8d0e25fa537a54
Nedim <mr.ramicevic@gmail.com>
2023-08-21 14:45:14 +0300

    contact vıew done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
@@ -11,7 +12,14 @@ import { ContactComponent } from './components/contact/contact.component';
 import { ProductComponent } from './components/product/product.component';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 
-
+const firebaseConfig = {
+  apiKey: "AIzaSyClv5PipgT55VSIn3MYVHhuuFQtDNSWYWI",
+  authDomain: "ecommerce-6d502.firebaseapp.com",
+  projectId: "ecommerce-6d502",
+  storageBucket: "ecommerce-6d502.appspot.com",
+  messagingSenderId: "37693060753",
+  appId: "1:37693060753:web:43aa53246184f02efe7130"
+};
 
 @NgModule({
   declarations: [
@@ -25,6 +33,8 @@ import { ProductDetailComponent } from './components/product-detail/product-deta
     ProductDetailComponent,
   ],
   imports: [
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirestore(() => getFirestore()),
     BrowserModule,
     AppRoutingModule
   ],
---
+++ src/app/services/firebase.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Firestore } from '@angular/fire/firestore';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FirebaseService {
+
+  constructor(private fs: Firestore) { }
+
+  getProducts(){
+    return true;
+  }
+}
