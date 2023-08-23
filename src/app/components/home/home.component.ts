e9e845da4195a7624f79086d0019b105147a7fdc
Nedim <mr.ramicevic@gmail.com>
2023-08-23 09:01:22 +0300

    qq

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -9,7 +9,17 @@ import { ProductsService } from '../../services/products.service';
 })
 export class HomeComponent implements OnInit   {
   products:any
+  selectedOption = '3'
 
+  onSelectChange(event:any){
+
+    if(event.target.value == '3 columns'){
+      this.selectedOption = '3'
+  }
+  else{
+    this.selectedOption = '4'
+  }
+}
   
   constructor (private service: ProductsService){}
 
